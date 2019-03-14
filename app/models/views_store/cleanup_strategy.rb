cd6b0ba0d62497298129d6fd6bb030934fe7a53b
Maksim V <hello.maksim.v@gmail.com>
2019-03-14 09:43:29 +0300

    Remove broken cleanup strategy

--- app/models/views_store/cleanup_strategy.rb
+++ app/models/views_store/cleanup_strategy.rb
@@ -10,15 +10,4 @@ module ViewsStore
       values.delete_if { |value_id, time| time < permissible_time }
     end
   end
-
-  class OptimizedCleanupStrategy < CleanupStrategy
-    def cleanup_hash(values, permissible_time)
-      hash = Hash.new
-      values.reverse_each do |value_id, time|
-        return if time < permissible_time
-        hash[value_id] = time
-      end
-      hash
-    end
-  end
 end
